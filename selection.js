2750dd13a6a5607463b6ad162a43dde3572a8150
MicroOptimization <jackiezhen538@gmail.com>
2023-04-22 01:24:32 -0400

    animations play in selection area now

--- selection.js
+++ selection.js
@@ -1,6 +1,12 @@
+import { constants } from './constants.js';
+
+let currentSpriteSet = constants.sprites.barry.active
+
 document.addEventListener("DOMContentLoaded", async () => {
     console.log("here");
     addSelectionListeners();
+    var spritePlayer = new SpritePlayer(currentSpriteSet)
+    spritePlayer.startSpriteLoop();
 });
 
 
@@ -18,3 +24,42 @@ function larrowHandler() {
 function rarrowHandler() {
     console.log("right");
 }
+
+
+
+class SpritePlayer {
+    constructor(spriteSet) {
+        this.img = document.getElementById("thumbnail");
+        this.curSpriteSet = spriteSet;
+        this.spriteIndex = "00";
+        this.maxSpriteIndex = this.curSpriteSet.frames;
+        
+        console.log("diag: " + this.curSpriteSet, this.maxSpriteIndex)
+    }
+
+    updateSprite() {
+        var newURLPath = this.curSpriteSet.path + this.spriteIndex + ".png"
+        console.log(newURLPath)
+        this.img.src = newURLPath;
+
+        this.spriteIndex = parseInt(this.spriteIndex)
+        this.spriteIndex++;
+        console.log(this.spriteIndex, this.maxSpriteIndex)
+        if (this.spriteIndex > this.maxSpriteIndex) {
+            this.spriteIndex = "00";
+        }
+        this.spriteIndex = this.spriteIndex.toString();
+
+        if (this.spriteIndex.length === 1) {
+            this.spriteIndex = "0" + this.spriteIndex;
+        }
+    }
+
+    startSpriteLoop() {
+        var fps = this.curSpriteSet.framerate;
+        var timeInterval = 1000 / fps; //this is how often we gotta update the loop to maintain our fps. //this is in ms, so 1000ms per second.
+        setInterval(this.updateSprite.bind(this), timeInterval);
+    }
+}
+
+
