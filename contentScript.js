23b3dbd0b8ae4a091f8b3f9986863f19e1946af8
MicroOptimization <jackiezhen538@gmail.com>
2023-05-18 01:13:56 -0400

    comments

--- contentScript.js
+++ contentScript.js
@@ -393,7 +393,7 @@
             bubbleDivExists = document.getElementById("bubble-div")
             timerLabelExists = document.getElementById("timer-label");
             console.log("here 3" , worldExists, bubbleExists, bubbleDivExists, timerLabelExists)
-            console.log("here 2" , worldExists === true , bubbleExists === true , bubbleDivExists === true , timerLabelExists === true)
+            //console.log("here 2" , worldExists === true , bubbleExists === true , bubbleDivExists === true , timerLabelExists === true)
             
             if (!new_timer_visibility && (worldExists) && bubbleExists && bubbleDivExists && timerLabelExists) {
                 console.log("here 1")
